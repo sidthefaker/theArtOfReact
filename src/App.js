52b2a5a0745247506b62dafbca029f51fb72991c
sidthefaker <sidthefaker@gmail.com>
2022-04-28 23:44:13 +0900

    study ch5.3 컴포턴트에 ref 달기  - 컴포넌트에 ref 달고 내부 메서드 사용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ValidationSample from './ValidationSample';
+import React, { Component } from 'react';
+import ScrollBox from './ScrollBox';
 
-const App = () => {
-  return <ValidationSample />;
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          to bottom
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
