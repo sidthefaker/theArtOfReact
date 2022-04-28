cc1d313bab7e9b6217bbd24364978ec076654ad3
sidthefaker <sidthefaker@gmail.com>
2022-04-28 23:22:32 +0900

    study ch5.2 ref 사용  - callback을 사용한 ref 적용

--- src/ValidationSample.js
+++ src/ValidationSample.js
@@ -16,15 +16,18 @@ class ValidationSample extends Component {
 
   handleButtonClick = () => {
     this.setState({
+      password: '',
       clicked: true,
       validated: this.state.password === '0000'
-    })
+    });
+    this.input.focus();
   }
 
   render() {
     return (
       <div>
         <input
+          ref={(ref) => this.input=ref}
           type='password'
           value={this.state.password}
           onChange={this.handleChange}
