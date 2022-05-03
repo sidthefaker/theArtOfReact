f8dbd70a20e74b4958daf3b0a7eb93337213d637
sidthefaker <sidthefaker@gmail.com>
2022-05-03 23:35:57 +0900

    study ch6.4 응용  - 데이터 추가 기능 구현  - 데이터 삭제 기능 구현

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 
 const IterationSample = () => {
-  const names = ['눈사람', '얼음','눈','바람']
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>)
+  const [names, setNames] = useState([
+    {id:1, text:'눈사람1'},
+    {id:2, text:'얼음2'},
+    {id:3, text:'눈3'},
+    {id:4, text:'바람4'}
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5);
+
+  const onChange = e => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText
+    });
+    setNextId(nextId + 1)
+    setNames(nextNames);
+    setInputText('');
+  }
+  const onRemove = id => {
+    const nextNames = names.filter(name => name.id !== id)
+    setNames(nextNames);
+  }
+
+  const nameList = names.map(name => <li key={name.id} onDoubleClick={() => onRemove(name.id)}>{name.text}</li>)
   return (
-    <ul>
-      {nameList}
-    </ul>
+    <>
+      <input value={inputText} onChange={onChange}/>
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </>
   );
 };
 
